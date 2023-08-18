078ac1ef780d8a1be1393570b9dcb179fab2ec35
chantellecv <84527073+chantellecv@users.noreply.github.com>
2023-08-18 15:48:36 +0000

    [LeetCode Sync] Runtime - 2 ms (55.56%), Memory - 41.1 MB (92.82%)

--- problems/0020-valid-parentheses/solution.java
+++ problems/0020-valid-parentheses/solution.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        
-        if (s.length() == 0 || s.length() %2 != 0){
-            return false;
+        Stack<Character> parStack = new Stack<>();
+        for (char c: s.toCharArray()){
+            if (c == '(' || c == '[' || c == '{'){
+                parStack.push(c);
+            }
+            else if (c == '}' && !parStack.empty() && parStack.peek() == '{'){
+                parStack.pop();
+            }
+            else if (c == ')' && !parStack.empty() && parStack.peek() == '('){
+                parStack.pop();
+            }
+            else if (c == ']'  && !parStack.empty() && parStack.peek() == '['){
+                parStack.pop();
+            }
+            else {return false;}
         }
-        Stack<Character> myStack = new Stack<>();
-        for (char i: s.toCharArray()){
-            if (i == '(' || i == '[' || i == '{'){
-                myStack.push(i);
-            }
-            else if (i == '}' && !myStack.empty() && myStack.peek()=='{'){
-                myStack.pop();
-            }
-            else if (i == ']' && !myStack.empty()  && myStack.peek()=='['){
-                myStack.pop();
-            }
-            else if (i == ')' && !myStack.empty()  && myStack.peek()=='('){
-                myStack.pop();
-            }
-            else myStack.push(i);
-        }
-        return myStack.empty();
-        
+        return parStack.empty();
     }
 }
