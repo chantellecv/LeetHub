cacebbd1903f586c3422e6424853b9ab74fd6869
chantellecv <84527073+chantellecv@users.noreply.github.com>
2023-09-18 09:33:24 +0000

    Sync LeetCode submission - Add Binary (java)

--- problems/add_binary/solution.java
+++ problems/add_binary/solution.java
@@ -1,12 +1,25 @@
-import java.math.BigInteger;
-
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger num1 = new BigInteger(a, 2);
-        BigInteger num2 = new BigInteger(b, 2);
+        int x = a.length() - 1;
+        int y = b.length() - 1;
 
-        BigInteger sum = num1.add(num2);
-        String ans = sum.toString(2);
-        return ans;
+        int sum = 0;
+        int carry = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (x >= 0 || y >= 0){
+            sum = carry;
+            if (x >= 0) sum += a.charAt(x) - '0';
+            if (y >= 0) sum += b.charAt(y) - '0';
+
+            carry = sum / 2;
+            sb.append(sum % 2);
+
+            x--;
+            y--;
+        }
+       if (carry > 0)  sb.append(carry);
+        return sb.reverse().toString();
     }
 }
